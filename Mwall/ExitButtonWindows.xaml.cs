4df5616a881eb795adaae087b5dec9865fd8cbc7
Nate <natecretech@outlook.com>
2016-11-12 13:58:46 +0400

    First Commit

---
+++ Mwall/ExitButtonWindows.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mwall
+{
+    /// <summary>
+    /// Interaction logic for ExitButtonWindows.xaml
+    /// </summary>
+    public partial class ExitButtonWindows : Window
+    {
+        private bool bMLeftPressed = false;
+        private bool bDragging = false;
+        private Point pointMDown = new Point();
+
+        public ExitButtonWindows()
+        {
+            InitializeComponent();
+
+            this.Top = 30;
+            double scrWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Left = scrWidth - this.Width;
+        }
+
+        private void ExitButtonWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            bMLeftPressed = true;
+            pointMDown.X = e.GetPosition(this).X;
+            pointMDown.Y = e.GetPosition(this).Y;
+        }
+
+        private void ExitButtonWindow_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(!bDragging) Application.Current.Shutdown();
+            bMLeftPressed = false;
+            bDragging = false;
+
+        }
+
+        private void ExitButtonWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!bMLeftPressed || MouseButtonState.Pressed != e.LeftButton) return;
+            if (Math.Abs(e.GetPosition(this).X - pointMDown.X) + Math.Abs(e.GetPosition(this).Y - pointMDown.Y) > 4)
+            {
+                bDragging = true;
+                DragMove();
+            }
+
+        }
+    }
+}
---
+++ Mwall/MColumn.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace Mwall
+{
+    class MColumn
+    {
+        string textSet; // the possible char set that appear in the column
+        Point startPoint; // the starting point coordination on the screen
+        Single fontSize; 
+        int verticalDist; // the space between two characters vertically
+
+        int scrHeight; // the screen's height in pixel
+        int columnHeight; // the column's height is screen height + current displaying text's (labels') height
+
+        int tickCount; // the update frequency of this column
+        int intervalCount; // the counter for frequency control
+        int len; // how many characters to be shown simultaneously
+        int cursor; // the cursor for which label is at the bottom of the column
+        Canvas canv; // store the canvas where the labels to be drawn
+        public static Random rd = new Random();
+        Label[] lbArr; // the label array
+        double[] opacityArr; // control the opacity for each label in the column
+        //static int lcmHeight = 1;
+
+        public static string[] PRETEXT = new string[] { "01", "0123456789",
+            "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ",  
+            "abcdefghijklmnopqrstuvwxyz0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            "abcdefghijklmnopqrstuvwxyz0123456789{}[]\\|<>?/~", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{}[]\\|<>?/~",
+            "!@#$%^&*()-=_+", "{}[]\\|<>?/~", "0123456789!@#$%^&*()-=_+{}[]\\|<>?/~"};
+
+        //static int GetGCD(int num1, int num2)
+        //{
+        //    while (num1 != num2)
+        //    {
+        //        if (num1 > num2) num1 = num1 - num2;
+        //        if (num2 > num1) num2 = num2 - num1;
+        //    }
+        //    return num1;
+        //}
+        //static int GetLCM(int num1, int num2) { return (num1 * num2) / GetGCD(num1, num2); }
+ 
+
+ 
+
+        public MColumn(string textStr, Point StartPoint, Single FontSize, int VerticalDistance, int screenHeight, int LenCount, int interval, Canvas BackgroundCanvas)
+        {
+            textSet = textStr;
+            startPoint = StartPoint;
+            fontSize = FontSize;
+            len = LenCount;
+            verticalDist = VerticalDistance;
+
+            scrHeight = screenHeight;
+            columnHeight = screenHeight + verticalDist * len;
+            //lcmHeight = GetLCM(lcmHeight, columnHeight);
+
+            intervalCount = interval;
+            canv = BackgroundCanvas;
+            cursor = len - 1;
+            tickCount = 0;
+
+            opacityArr = new double[len];
+            double opaDelta = 1f / len;
+            for (int i = 0; i < len; i++)
+                opacityArr[i] = (i + 1) * opaDelta;
+
+            lbArr = new Label[len];
+            for (int i = 0; i < len; i++)
+            {
+                Label l = new Label();
+                //l.ReleaseMouseCapture();
+                l.Content = textSet.Substring(rd.Next(textSet.Length),1);
+                l.FontSize = fontSize;
+                l.Foreground = new SolidColorBrush(Colors.ForestGreen);
+                BlurEffect b = new BlurEffect();
+                b.Radius = 2;
+                l.Effect = b;
+                l.Opacity = opacityArr[i];
+                //l.Margin = new Thickness(startPoint.X, (startPoint.Y + i * verticalDist) % scrHeight, 0, 0);
+                l.Margin = new Thickness(startPoint.X, (startPoint.Y + i * verticalDist) % columnHeight, 0, 0);
+                BackgroundCanvas.Children.Add(l);
+                lbArr[i] = l;
+            }
+        }
+
+        public int Draw()
+        {
+            //tickCount++;
+            if (++tickCount < intervalCount) return 0;
+            tickCount = 0;
+            //if (0 != tickCount % intervalCount) return 0;
+
+            cursor = (cursor + 1) % len;
+            Label mlb = lbArr[cursor];
+            mlb.Content = textSet.Substring(rd.Next(textSet.Length),1);
+            double oldx = mlb.Margin.Left;
+            double oldy = mlb.Margin.Top;
+            //mlb.Margin = new Thickness(startPoint.X, (startPoint.Y + (tickCount / intervalCount + len - 1) * verticalDist) % scrHeight, 0, 0);
+            //mlb.Margin = new Thickness(oldx, (oldy + len * verticalDist) % scrHeight, 0, 0);
+            mlb.Margin = new Thickness(oldx, (oldy + len * verticalDist) % columnHeight, 0, 0);
+            mlb.Opacity = opacityArr[len - 1];
+            for(int j = 1; j < len; j++)
+            {
+                int i = (cursor + j) % len;
+                Label clb = lbArr[i];
+                clb.Opacity = opacityArr[j - 1];
+                //if(3 == j) clb.Content = textSet.Substring(rd.Next(textSet.Length),1);
+                //clb.Margin = new Thickness(startPoint.X, startPoint.Y + (tickCount / intervalCount + j) * verticalDist, 0, 0);
+
+            }
+            return 0;
+        }
+    }
+}

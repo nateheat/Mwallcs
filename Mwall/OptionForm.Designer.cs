8167f06f20ade177f95e98f9aa0033b564de96cb
Nate <natecretech@outlook.com>
2016-11-27 11:19:23 +0400

    minor add some control on option form

--- Mwall/OptionForm.Designer.cs
+++ Mwall/OptionForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.buttonOK = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBoxStyle = new System.Windows.Forms.GroupBox();
+            this.radioButtonComet = new System.Windows.Forms.RadioButton();
+            this.radioButtonStraight = new System.Windows.Forms.RadioButton();
+            this.groupBoxStyle.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonOK
@@ -50,15 +54,51 @@
             this.buttonCancel.Text = "&Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
+            // groupBoxStyle
+            // 
+            this.groupBoxStyle.Controls.Add(this.radioButtonStraight);
+            this.groupBoxStyle.Controls.Add(this.radioButtonComet);
+            this.groupBoxStyle.Location = new System.Drawing.Point(12, 47);
+            this.groupBoxStyle.Name = "groupBoxStyle";
+            this.groupBoxStyle.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxStyle.TabIndex = 2;
+            this.groupBoxStyle.TabStop = false;
+            this.groupBoxStyle.Text = "Dropping Style";
+            // 
+            // radioButtonComet
+            // 
+            this.radioButtonComet.AutoSize = true;
+            this.radioButtonComet.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonComet.Name = "radioButtonComet";
+            this.radioButtonComet.Size = new System.Drawing.Size(75, 17);
+            this.radioButtonComet.TabIndex = 0;
+            this.radioButtonComet.TabStop = true;
+            this.radioButtonComet.Text = "Comet Tail";
+            this.radioButtonComet.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonStraight
+            // 
+            this.radioButtonStraight.AutoSize = true;
+            this.radioButtonStraight.Location = new System.Drawing.Point(7, 44);
+            this.radioButtonStraight.Name = "radioButtonStraight";
+            this.radioButtonStraight.Size = new System.Drawing.Size(85, 17);
+            this.radioButtonStraight.TabIndex = 1;
+            this.radioButtonStraight.TabStop = true;
+            this.radioButtonStraight.Text = "Straight Text";
+            this.radioButtonStraight.UseVisualStyleBackColor = true;
+            // 
             // OptionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 275);
+            this.Controls.Add(this.groupBoxStyle);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.Name = "OptionForm";
             this.Text = "OptionForm";
+            this.groupBoxStyle.ResumeLayout(false);
+            this.groupBoxStyle.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -67,5 +107,8 @@
 
         private System.Windows.Forms.Button buttonOK;
         private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.GroupBox groupBoxStyle;
+        private System.Windows.Forms.RadioButton radioButtonStraight;
+        private System.Windows.Forms.RadioButton radioButtonComet;
     }
 }
